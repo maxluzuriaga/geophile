5858db7c78aa805c6f467422c29e369cd746fb20
Max Luzuriaga <max@luzuriaga.com>
2014-11-15 01:02:58 -0500

    Sessions and stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+    def authenticate
+      unless logged_in?
+        redirect_to login_path, :notice => "Please sign in"
+      end
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+	def sign_in(user)
+		session[:user_id] = user.user_id
+		current_user = user
+	end
+
+	def curent_user=(user)
+		@current_user = user
+	end
+
+	def sign_out
+		session[:user_id] = nil
+		current_user = nil
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
 end
